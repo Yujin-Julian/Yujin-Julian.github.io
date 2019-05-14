566fbf5df89a1291e9501b5c63b0cfa1b8812336
Yujin-Julian Litz Park <32524692+Yujin-Julian@users.noreply.github.com>
2019-05-13 20:45:50 -0700

    New Site Update!

---
+++ subscribe.js
@@ -0,0 +1,41 @@
+function getUrlParameter(name) { /*Get the parameters in the url bar*/
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+var param = getUrlParameter("type");
+if (param != null && param.toUpperCase()==="BASIC"){
+	$(document).ready(function(){ 
+    $("#basic").show()
+
+});
+
+}
+else if(param != null && param.toUpperCase()==="STANDARD"){
+$(document).ready(function(){ /*wait until webpage content is ready and after it has loaded, show the corresponding html code based on what the user has clicked previously.*/
+    $("#standard").show()
+
+});
+}
+else if(param != null && param.toUpperCase()==="PREMIUM"){
+	$(document).ready(function(){ 
+    $("#premium").show()
+
+});
+}
+
+else{
+	$(document).ready(function(){ 
+    $("#regular").show()
+
+});
+
+}
+
+
+
+
+
+
